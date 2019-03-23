2fd90e35181e971b271412d6922b9296551fc6e0
lxc <liuxcc@163.com>
2019-03-23 09:27:34 +0800

    tiny fix

--- train.sh
+++ train.sh
@@ -3,42 +3,12 @@ export MXNET_CPU_WORKER_NTHREADS=24
 export MXNET_CUDNN_AUTOTUNE_DEFAULT=1
 export MXNET_ENGINE_TYPE=ThreadedEnginePerDevice
 
-# NETWORK='sdu'
-# MODELDIR='./model/model-sdu-3d/3'
-# mkdir -p "$MODELDIR"
-# PREFIX="$MODELDIR/model"
-# PRETRAINED="./model/model-sdu-3d/2/model,35"
-# LOGFILE="$MODELDIR/log"
-
-# CUDA_VISIBLE_DEVICES='0' python -u train.py --network $NETWORK --prefix "$PREFIX" --pretrained $PRETRAINED --per-batch-size 16 --lr 2e-5 --lr-step '16000,24000,30000' > "$LOGFILE" 2>&1 &
-
 
 NETWORK='sdu'
-MODELDIR='./model/model-sat2d3-cab/2'
+MODELDIR='./model/model-sat2d3-cab/'
 mkdir -p "$MODELDIR"
 PREFIX="$MODELDIR/model"
-PRETRAINED="./model/model-sat2d3-cab/1/model,150"
+PRETRAINED="./model/model-sat2d3-cab/model,0"
 LOGFILE="$MODELDIR/log"
 
-CUDA_VISIBLE_DEVICES='0' python -u train.py --network $NETWORK --prefix "$PREFIX" --pretrained $PRETRAINED --per-batch-size 16 --lr 1e-4 --lr-epoch-step '1,2,3' > "$LOGFILE" 2>&1 &
-
-
-
-# NETWORK='hourglass'
-# MODELDIR='./model/model-hg2d3-cab-l2/3'
-# mkdir -p "$MODELDIR"
-# PREFIX="$MODELDIR/model"
-# PRETRAINED="./model/model-hg2d3-cab-l2/2/model,79"
-# LOGFILE="$MODELDIR/log"
-
-# CUDA_VISIBLE_DEVICES='0' python -u train.py --network $NETWORK --prefix "$PREFIX" --pretrained $PRETRAINED --per-batch-size 24 --lr 1e-6 --lr-step '16000,24000,30000' > "$LOGFILE" 2>&1 &
-
-
-# NETWORK='hourglass'
-# MODELDIR='./model/model-hg2d3-hpm/3'
-# mkdir -p "$MODELDIR"
-# PREFIX="$MODELDIR/model"
-# PRETRAINED="./model/model-hg2d3-hpm/2/model,38"
-# LOGFILE="$MODELDIR/log"
-
-# CUDA_VISIBLE_DEVICES='0' python -u train.py --network $NETWORK --prefix "$PREFIX" --pretrained $PRETRAINED --per-batch-size 25 --lr 1e-6 --lr-step '16000,24000,30000' > "$LOGFILE" 2>&1 &
+CUDA_VISIBLE_DEVICES='0' python -u train.py --network $NETWORK --prefix "$PREFIX" --pretrained $PRETRAINED --per-batch-size 16 --lr 1e-6 --lr-epoch-step '20,35,45' > "$LOGFILE" 2>&1 &
